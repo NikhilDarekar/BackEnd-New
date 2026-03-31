b82ba7e9d32e84342208d772a5357c606ef56321
NikhilDarekar <nikhilx97@gmail.com>
2026-03-31 16:15:25 +0530

    successfully connected to mongoose database

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import mongoose from "mongoose"
-import {DB_NAME} from "./constants"
+import dotenv from 'dotenv';
+// dotenv.config({ path: './.env' });
+import connectDB from "./db/index.js"
 
+dotenv.config({
+    path: './.env'
+})
 
+connectDB()
 
 
 
